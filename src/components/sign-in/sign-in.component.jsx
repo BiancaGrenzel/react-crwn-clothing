fa8d139cc2a960ea3e971c24bf0255ebbcf664fd
bianca.severo <bianca.severo@softplan.com.br>
2021-03-21 22:07:48 -0300

    Aula 87 - Custom Button Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import FormInput from '../form-input/form-input.component'
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -36,7 +37,7 @@ class SignIn extends React.Component{
                     <FormInput name='email' type='email' value={this.state.email} label="email" handleChange={this.handleChange} required></FormInput>
                     <FormInput name='password' type='password' value={this.state.password} label="password" handleChange={this.handleChange} required></FormInput>
 
-                    <input type='submit' value='Submit Form'></input>
+                    <CustomButton type='submit'>Sign in</CustomButton>
                 </form>
             </div>
         )
